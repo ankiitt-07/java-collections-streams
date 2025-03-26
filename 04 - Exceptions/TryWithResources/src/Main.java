b7c4acd13bd3b8371b76f370bc7756067efda515
ankiitt-07 <ak077636@gmail.com>
2025-03-26 10:48:26 +0530

    [Ankit] Add: try-with-resources (Auto-closing Resources)

---
+++ 04 - Exceptions/TryWithResources/src/Main.java
@@ -0,0 +1,8 @@
+import com.file_handler.Reader;
+public class Main {
+    public static void main(String[] args) {
+        Reader.readFile("src/com/content/info.txt");
+//        Reader.readFile("Hello.txt");
+    }
+
+}
