1fd1c017b0762dc2371513fb2b9f05fb416485bd
ankiitt-07 <ak077636@gmail.com>
2025-03-27 09:59:51 +0530

    [Ankit] Add: Database Performance Test

---
+++ 05 - Regex & JUnit/JUnit/src/main/java/com/testing/db_connection/DatabaseConnection.java
@@ -0,0 +1,28 @@
+package com.testing.db_connection;
+
+public class DatabaseConnection {
+    private boolean connected = false;
+
+    public void connect() {
+        System.out.println("Connecting to the database...");
+        connected = true;
+    }
+
+    public void disconnect() {
+        System.out.println("Disconnecting from the database...");
+        connected = false;
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
+
+    public boolean isConnectedDelayed(){
+        try{
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+        return connected;
+    }
+}
